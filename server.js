99835408d0dd0629a8fb9318e56371e7f70f9116
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-10 18:21:09 +0200

    Create routes for getting and updating profiles/profile

--- server.js
+++ server.js
@@ -14,10 +14,44 @@ app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
+app.get("/profiles", async (req, res) => {
+	try {
+		const allProfiles = await Profile.find({});
+		res.status(200).json(allProfiles);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/profiles/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const profile = await Profile.findById(id);
+		res.status(200).json(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+});
+
 app.post("/profile", async (req, res) => {
 	try {
 		const profile = await Profile.create(req.body);
 		res.status(200).json(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.put("/profiles/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const profile = await Profile.findByIdAndUpdate(id, req.body);
+		if (!profile) {
+			res.status(404).json({ message: "Profile not found" });
+		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: error.message });
