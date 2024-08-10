e5935cdcaadcb0b9865e5f54a7dc87650137463f
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-10 18:12:35 +0200

    Enable saving profiles to database

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const mongodbConfig = require("./mongodbConfig");
 
 const express = require("express");
 const mongoose = require("mongoose");
+const Profile = require("./models/profileModel");
 const app = express();
+
+app.use(express.json());
 
 const databaseConnectionString = mongodbConfig.mongodbConnectionString;
 
 // Routes
 app.get("/", (req, res) => {
 	res.send("Hello World");
+});
+
+app.post("/profile", async (req, res) => {
+	try {
+		const profile = await Profile.create(req.body);
+		res.status(200).json(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
 });
 
 mongoose
