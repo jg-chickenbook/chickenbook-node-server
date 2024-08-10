2f537d924ffd78428ce21239fae07fbe8b4d99e9
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-10 18:27:35 +0200

    Create and use profileController

---
+++ controllers/profileController.js
@@ -0,0 +1,52 @@
+const Profile = require("../models/profileModel");
+
+const getAllProfiles = async (req, res) => {
+	try {
+		const allProfiles = await Profile.find({});
+		res.status(200).json(allProfiles);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const getProfileById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const profile = await Profile.findById(id);
+		res.status(200).json(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const createProfile = async (req, res) => {
+	try {
+		const profile = await Profile.create(req.body);
+		res.status(200).json(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const updateProfile = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const profile = await Profile.findByIdAndUpdate(id, req.body);
+		if (!profile) {
+			res.status(404).json({ message: "Profile not found" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = {
+	getAllProfiles,
+	getProfileById,
+	createProfile,
+	updateProfile,
+};
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const mongodbConfig = require("./mongodbConfig");
+const {
+	getAllProfiles,
+	getProfileById,
+	createProfile,
+	updateProfile,
+} = require("./controllers/profileController");
 
 const express = require("express");
 const mongoose = require("mongoose");
-const Profile = require("./models/profileModel");
 const app = express();
 
 app.use(express.json());
@@ -14,49 +19,11 @@ app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.get("/profiles", async (req, res) => {
-	try {
-		const allProfiles = await Profile.find({});
-		res.status(200).json(allProfiles);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: error.message });
-	}
-});
-
-app.get("/profiles/:id", async (req, res) => {
-	try {
-		const { id } = req.params;
-		const profile = await Profile.findById(id);
-		res.status(200).json(profile);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: error.message });
-	}
-});
-
-app.post("/profile", async (req, res) => {
-	try {
-		const profile = await Profile.create(req.body);
-		res.status(200).json(profile);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: error.message });
-	}
-});
-
-app.put("/profiles/:id", async (req, res) => {
-	try {
-		const { id } = req.params;
-		const profile = await Profile.findByIdAndUpdate(id, req.body);
-		if (!profile) {
-			res.status(404).json({ message: "Profile not found" });
-		}
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: error.message });
-	}
-});
+// Profile routes
+app.get("/profiles", getAllProfiles);
+app.get("/profiles/:id", getProfileById);
+app.post("/profile", createProfile);
+app.put("/profiles/:id", updateProfile);
 
 mongoose
 	.connect(databaseConnectionString)
