0558c0ec756117dd4b31dacde26667fcf455115a
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-10 17:45:57 +0200

    Connect to Mongo DB using Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+const mongodbConfig = require("./mongodbConfig");
+
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const databaseConnectionString = mongodbConfig.mongodbConnectionString;
 
 // Routes
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.listen(3000, () => {
-	console.log("Node API app is running on http://localhost:3000/");
-});
+mongoose
+	.connect(databaseConnectionString)
+	.then(() => {
+		console.log("Connected to MongoDB");
+		app.listen(3000, () => {
+			console.log(
+				"Chickenbook Server app is running on http://localhost:3000/",
+			);
+		});
+	})
+	.catch((err) => {
+		console.log("Error connecting to MongoDB", err);
+	});
