5f2a4b4d0eb2bb9c108a7c834b8634b8654e95cf
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-10 16:42:08 +0200

    Use nodemon and setup root route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// Routes
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
 app.listen(3000, () => {
-	console.log("Node API app is running on port 3000");
+	console.log("Node API app is running on http://localhost:3000/");
 });
