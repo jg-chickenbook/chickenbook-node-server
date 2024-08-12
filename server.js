ceb7472bb83722738cd0eb7909fc5ca0820154fe
Miloslav Jezek <milo.jezek@gmail.com>
2024-08-12 17:23:49 +0200

    Modify server to include CORS headers

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const {
 } = require("./controllers/profileController");
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const databaseConnectionString = mongodbConfig.mongodbConnectionString;
